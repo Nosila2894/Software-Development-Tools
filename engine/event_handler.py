12e386191b92b000d12ecf046369ece49dc44c14
Alison Rea <siralex2894@hotmail.com>
2026-07-18 03:35:55 -0400

    Commented the event handler class to prepare for a test file. Created the shell for the Event Handler test suite.Updated gitignore to remove Personal Game Project log and main as they are part of a game project and are not helpful in highlighting my application tools functionality.

--- engine/event_handler.py
+++ engine/event_handler.py
@@ -1,9 +1,11 @@
 from collections import deque
 
 class EventHandler():
+    #Implemented a deque data structure for left hand pops from the queue
     def __init__(self):
         self._queue = deque()
-
+    
+    #Provides an interface for the states to load and extract action commands.
     def push_event(self, action):
         self._queue.append(action)
 
@@ -12,12 +14,16 @@ class EventHandler():
             return self._queue.popleft()
         return None
 
+    # Allow states to verify any pending actions remain in the handler
     def has_events(self):
         return bool(self._queue)
     
     def clear_events(self):
         self._queue.clear()
-
+    
+    # Executed during the update function of a state to clear the event handler
+    # Allows game logic to update while not clogging the event handler and 
+    # Freezing the application
     def extract_all_events(self):
         events = list(self._queue)
         self.clear_events()
---
+++ tests/test_event_handler.py
@@ -0,0 +1,9 @@
+import unittest
+from engine import EventHandler
+
+class Test_test_event_handler(unittest.TestCase):
+    def test_A(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
